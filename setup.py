61173b07ead7436a9fe8c604b2401562fbfcb9c5
Max Countryman <max.countryman@gmail.com>
2011-12-06 14:58:27 -0500

    changing scope for TESTING bailout, bumping version 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SeaSurf',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/maxcountryman/flask-seasurf/',
     license='BSD',
     author='Max Countryman',
