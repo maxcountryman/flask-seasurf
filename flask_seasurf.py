d1c4ded64f975f357a9b42dea9edea8c078c51ec
Alan Hamlett <alan.hamlett@gmail.com>
2013-07-11 01:03:40 -0700

    using correct variable name

--- flask_seasurf.py
+++ flask_seasurf.py
@@ -170,7 +170,7 @@ class SeaSurf(object):
         '''Given a view function, determine whether or not we should
         deliver a CSRF token to this view through the response and
         validate CSRF tokens upon requests to this view.'''
-        if view is None:
+        if view_func is None:
             return False
         view = '%s.%s' % (view_func.__module__, view_func.__name__)
         if self._type == 'exempt':
